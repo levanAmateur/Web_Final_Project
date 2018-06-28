0b752c51584fa80e17530de92b881248894f5811
levanAmateur <lkara15@freeuni.edu.ge>
2018-06-28 22:01:25 +0400

    shevqmeni folderi saxelad app. app-shi aris folderebi css da scss. failebi: index.html da index.js

---
+++ app/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  	fs.readFile('./app/index.html', null, function(error, data) {
+  	if(error) {
+  		res.writeHead('404');
+  		res.write('File not found!');
+  	} else {
+  		res.write(data);
+  	}
+  	res.end();
+  });
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
