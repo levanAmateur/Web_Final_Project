d326fc943cfa0fb963102865254cf7c895563aaf
levanAmateur <lkara15@freeuni.edu.ge>
2018-07-06 23:57:39 +0400

    changed css

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,13 +1,10 @@
 var matchInfo = document.querySelector('.middle');
 
-
-
 var sidebarCont = document.querySelector('.a');
 var gameBar = sidebarCont.querySelector('.c');
 gameBar.querySelector('.date').innerHTML = Games[0].date;
 gameBar.querySelector('.game').innerHTML = Games[0].p1name + " - " + Games[0].p2name;
 gameBar.id = "gameBar-0";
-
 
 function listeners(){
 	for(var i = 0; i<Games.length; i++){
@@ -39,26 +36,8 @@ function gamesList(){
 	})
 }
 
+
+
+
 gamesList();
 listeners();
-
-/*
-
-
-
-
-
-var frinedsBar = document.querySelector('.friends-bar');
-var friend = frinedsBar.querySelector('.friend');
-friend.querySelector('.fr-bar-text').innerHTML = friends[0].name;
-friend.querySelector('.fr-bar-avatar').src = friends[0].img;
-
-function friendsList(data, x){
-	data.forEach(function(elem, idx){
-		if(idx == x) return;
-		var clnNode = friend.cloneNode(true);
-		clnNode.querySelector('.fr-bar-text').innerHTML = elem.name;
-		clnNode.querySelector('.fr-bar-avatar').src = elem.img;
-		frinedsBar.appendChild(clnNode);
-	})
-}*/
